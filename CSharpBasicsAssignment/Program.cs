bb602a4f0e9c89f44c93c0dde184c153fa97cc11
Omar Salem <omarsalemabdelrouf@gmail.com>
2026-09-14 09:47:33 +0300

    add part-A: Project & Structure

---
+++ CSharpBasicsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿
+/*
+ * 1- csproj => this is a project configuration blueprint | build setting and project reference
+ *
+ * 2- program.cs => Source code
+ *  contain main app logic and top-level statements
+ * serves as the primary entry point where program execution begins when running the compiled executable
+ * 
+ * 3- obj => Temp build directory
+ * holds temp files generated during the compilation process
+ *
+ * 4- bin => out directory
+ * holds the final compiled out of your project => .ex
+ * 
+ */
+namespace CSharpBasicsAssignment;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+ 
